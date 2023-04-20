f6970349875a26096c6ea4b1e16207b8613e339f
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-20 17:52:22 +0200

    add function

--- 1-palindroma/js/script.js
+++ 1-palindroma/js/script.js
@@ -1,4 +1,15 @@
 /*Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
-const word = prompt('Inserisci una parola');
+const word = prompt("Inserisci una parola palindroma:");
+
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+};
+
+if (isPalindrome(word)) {
+  console.log(`${word} è una parola palindroma`);
+} else {
+  console.log(`${word} non è una parola palindroma`);
+};
